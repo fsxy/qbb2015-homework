688526be08e7b98afda7ba7a4183b2cf34bce149
fsxy <fsxy2011@gmail.com>
2015-08-25 13:18:14 -0400

    2.py

---
+++ day2-lunch/basic2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+filename="/Users/cmdb/qbb2015/day1/SRR072893.sam"
+f=open(filename)
+
+count=0
+for i in f:
+    if "HO" in i:
+        count+=1
+        
+print "Number of perfect hits:", count
