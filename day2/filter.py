748393c7aaa453708c9f00bc07c0e44a256043d6
fsxy <fsxy2011@gmail.com>
2015-08-25 11:24:30 -0400

    Filter lines 10 through 15

--- day2/filter.py
+++ day2/filter.py
@@ -2,9 +2,22 @@
 
 filename="/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 
+#f=open(filename)
+
+#for line in f:
+#    fields=line.split()       #fields is now a list. if printed, it looks like this : ['abc' 'ddd' 'dds' 'sfe'] 
+#    if "tRNA" in fields[9]:
+#        print line,          #the comma stops adding an extra new line by print. because we already have a new line in the original file.
+        
+#f.close()
+
 f=open(filename)
 
-for line in f:
-    fields=line.split()       #fields is now a list. if printed, it looks like this : ['abc' 'ddd' 'dds' 'sfe'] 
-    if "tRNA" in fields[9]:
-        print line,          #the comma stops adding an extra new line by print. because we already have a new line in the original file.
+for line_count, line in enumerate(f):        
+    if line_count<=10:
+        pass
+    elif line_count<=15:
+        print line,
+    else:
+        break
+    
