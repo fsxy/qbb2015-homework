12e506cd3502e82c391de9397435ae965aa43b86
fsxy <fsxy2011@gmail.com>
2015-08-29 21:18:43 -0400

    day2-homework

--- day2-homework/exercise4.py
+++ day2-homework/exercise4.py
@@ -9,16 +9,20 @@ df=pd.read_table(annotation)
 
 df.columns=["t_id","chr","strand","start","end","tname","num_exons","length","gene_id","gene_name","cov","FPKM"]
 
-plotdf=df.sort("FPKM",ascending=False)
+plotdf=df.sort("FPKM",ascending=False).reset_index()
+filter_zero=plotdf["FPKM"]>0
+plotdf_no_zero=plotdf[filter_zero]
 
-rows=plotdf.shape[0]
-print rows
+
+rows=plotdf_no_zero.shape[0]
+
 checkpoint1=int(rows/3)
 checkpoint2=2*checkpoint1
-
+#print plotdf_no_zero["FPKM"]
+#print plotdf_no_zero
 plt.figure()
-#plt.boxplot(plotdf["FPKM"][0:checkpoint1])
-plt.boxplot(df["FPKM"][0:checkpoint1])
-plt.title("Top 1 3rd of FPKM")
-plt.savefig("FPKM_boxplot_3_1.png")
+plt.boxplot([plotdf_no_zero["FPKM"][:checkpoint1],plotdf_no_zero["FPKM"][checkpoint1:checkpoint2],plotdf_no_zero["FPKM"][checkpoint2:]])
+
+plt.title("Boxplot of PKM")
+plt.savefig("FPKM_boxplot.png")
 
